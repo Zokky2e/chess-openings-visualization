c61eb9c899be29b37a4ad409823e379e82749fea
Zokky2e <icro4.gaming@hotmail.com>
2023-05-11 22:55:09 +0200

    testint stuff

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as d3 from "d3";
 import { ChessOpening } from "../models/ChessOpening";
 import { startingChessBoardTiles } from "../models/StartingChessBoardTiles";
+import { useEffect } from "react";
 interface ChessBoardProps {
 	chessOpening: ChessOpening;
 }
@@ -15,20 +17,24 @@ function ChessBoard(chessBoardProps: ChessBoardProps) {
 			.append("svg")
 			.attr("id", "chess-board")
 			.attr("width", boardSize)
-			.attr("height", boardSize);
+			.attr("height", boardSize)
+			.attr("viewBox", `0 0 ${boardSize} ${boardSize}`);
 		const columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
 		const rows = ["8", "7", "6", "5", "4", "3", "2", "1"];
 		for (let i = 0; i < 8; i++) {
 			for (let j = 0; j < 8; j++) {
+				const parentX = i * squareSize;
+				const parentY = j * squareSize;
+				let piece = startingChessBoardTiles.find(
+					(tile) => tile.id === `${columns[i]}${rows[j]}`
+				)?.piece;
+				const id = `${columns[i]}${rows[j]}`;
 				board
 					.append("rect")
 					.attr("id", () => {
-						return `${columns[i]}${rows[j]}`;
+						return id;
 					})
 					.attr("piece", () => {
-						const piece = startingChessBoardTiles.find(
-							(tile) => tile.id === `${columns[i]}${rows[j]}`
-						)?.piece;
 						return piece ? piece : "";
 					})
 					.attr("x", i * squareSize)
@@ -36,6 +42,33 @@ function ChessBoard(chessBoardProps: ChessBoardProps) {
 					.attr("width", squareSize)
 					.attr("height", squareSize)
 					.attr("fill", (i + j) % 2 === 0 ? "#ecdab8" : "#ae8a67");
+				if (!piece) {
+					piece = "";
+				}
+				switch (piece.toLowerCase()) {
+					case "p": {
+						drawPawn(id, piece, parentX, parentY);
+						break;
+					}
+					case "n": {
+						break;
+					}
+					case "r": {
+						break;
+					}
+					case "b": {
+						break;
+					}
+					case "q": {
+						break;
+					}
+					case "k": {
+						break;
+					}
+					default: {
+						break;
+					}
+				}
 				if (j === 7) {
 					board
 						.append("text")
@@ -63,84 +96,81 @@ function ChessBoard(chessBoardProps: ChessBoardProps) {
 	}
 
 	function drawPawn(
-		d: d3.Selection<
-			SVGSVGElement,
-			{ id: string; piece: string },
-			HTMLElement,
-			any
-		>
+		id: string,
+		piece: string,
+		parentX: number,
+		parentY: number
 	) {
-		const pawnGroup = d.append("g");
+		const pawnX = parentX + 20;
+		const pawnY = parentY + 50;
+		const pawnWidth = 50;
+		const pawnHeight = 30;
+		const svg = d3
+			.select("#chess-board")
+			.insert("g", ":first-child")
+			.attr("id", `${piece}${id}`)
+			.attr("width", "150px")
+			.attr("height", "150px");
 
-		// create the pawn shape as a circle
-		pawnGroup
-			.append("circle")
-			.attr("r", 10)
-			.attr("fill", "white")
-			.attr("stroke", "black")
-			.attr("stroke-width", 2);
+		const pawnBody = svg
+			.append("rect")
+			.attr("x", pawnX)
+			.attr("y", pawnY)
+			.attr("id", `body-${piece}${id}`)
+			.attr("width", pawnWidth - 10)
+			.attr("height", pawnHeight - 20)
+			.attr("rx", 5)
+			.style("fill", piece === "P" ? "white" : "black");
 
-		// position the pawn on the board
-		pawnGroup
-			.attr("transform", `translate(100, 400)`)
-			.style("z-index", 1000);
+		const pawnHead = svg
+			.append("rect")
+			.attr("x", pawnX + 5)
+			.attr("y", pawnY - 10)
+			.attr("id", `head-${piece}${id}`)
+			.attr("width", pawnWidth - 30)
+			.attr("height", pawnHeight - 20)
+			.style("fill", piece === "P" ? "white" : "black");
+
+		const pawnTop = svg
+			.append("rect")
+			.attr("x", pawnX + 10)
+			.attr("y", pawnY - 20)
+			.attr("id", `top-${piece}${id}`)
+			.attr("width", pawnWidth - 20)
+			.attr("height", pawnHeight - 20)
+			.style("fill", piece === "P" ? "white" : "black");
 	}
 
-	function drawChessPieces() {
-		const pieces = d3
-			.selectAll<SVGSVGElement, { id: string; piece: string }>(
-				"rect[piece]"
-			)
-			.append("svg")
-			.attr("id", "chess-piece")
-			.attr("width", squareSize)
-			.attr("height", squareSize)
-			.call(function (
-				this: d3.Selection<
-					SVGSVGElement,
-					{ id: string; piece: string },
-					HTMLElement,
-					any
-				>,
-				selection
-			) {
-				selection.each(function (d) {
-					let piece = "";
-					if (!d3.select(this).attr("piece")) {
-						piece = "";
-					} else {
-						piece = d3.select(this).attr("piece").toLowerCase();
-					}
-					switch (piece) {
-						case "p": {
-							drawPawn(this);
-							break;
-						}
-						case "n": {
-							break;
-						}
-						case "r": {
-							break;
-						}
-						case "b": {
-							break;
-						}
-						case "q": {
-							break;
-						}
-						case "k": {
-							break;
-						}
-						default: {
-							break;
-						}
-					}
-				});
-			});
-	}
-	drawChessBoard();
-	drawChessPieces();
+	useEffect(() => {
+		drawChessBoard();
+	}, []);
 	return <div></div>;
 }
 
 export default ChessBoard;
+
+// switch (d.parent) {
+// 	case "p": {
+// 		console.log(d.id);
+// 		drawPawn();
+// 		break;
+// 	}
+// 	case "n": {
+// 		break;
+// 	}
+// 	case "r": {
+// 		break;
+// 	}
+// 	case "b": {
+// 		break;
+// 	}
+// 	case "q": {
+// 		break;
+// 	}
+// 	case "k": {
+// 		break;
+// 	}
+// 	default: {
+// 		break;
+// 	}
+// }
