5e775d7a5b77b9b7645ffc2965c85c8cf88513d4
Zokky2e <icro4.gaming@hotmail.com>
2023-06-01 21:09:23 +0200

    pawn logic done

--- src/hooks/useChessboardMove.ts
+++ src/hooks/useChessboardMove.ts
@@ -23,23 +23,44 @@ function GetChessboardMove(cmp: ChessBoardMoveProps) {
 		} else {
 			moves = currentMove.split(/(?=[a-z])/);
 			if(moves.length === 1) {
-				moves.unshift("");
+				moves.unshift("P");
 			}
-			if(moves[0] === "")
-					moves[0] = "P";
-				if(isBlack)
-					moves[0] = moves[0].toLowerCase();
+			console.log(moves);
+			moves = moves.filter(move => move !== "x");
+			console.log(moves);
+			if(moves[0] === moves[0].toLowerCase()) {
+				moves[0] = "P"+moves[0];
+			}
+			console.log(moves);
 			if(moves.length === 2) {
-				switch(moves[0].toLowerCase()) {
+				switch(moves[0][0].toLowerCase()) {
 					case "p": {
-						const fromTile = newBoardState.findIndex(tile => {
-							return tile.id[0] === moves[1][0] && tile.piece === moves[0]
-						})
-						const toTile = newBoardState.findIndex(tile => {
-							return tile.id === moves[1];
-						})
-						newBoardState[fromTile].piece = "";
-						newBoardState[toTile].piece = moves[0];
+						if(moves[0].length === 1) {
+							moves[0] = moves[0]+moves[1][0];
+						}
+						if(isBlack)
+							moves[0] = moves[0].toLowerCase();
+						console.log(moves);
+						if(moves[1][0] !== moves[0][1]) {
+							const fromTile = newBoardState.findIndex(tile => {
+								return tile.id[0] === moves[0][1] && tile.piece === moves[0][0];
+							});
+							const toTile = newBoardState.findIndex(tile => {
+								return tile.id === moves[1];
+							});
+							newBoardState[fromTile].piece = "";
+							newBoardState[toTile].piece = moves[0][0];
+						}
+						else {
+							const fromTile = newBoardState.findIndex(tile => {
+								return tile.id[0] === moves[1][0] && tile.piece === moves[0][0];
+							});
+							const toTile = newBoardState.findIndex(tile => {
+								return tile.id === moves[1];
+							});
+							newBoardState[fromTile].piece = "";
+							newBoardState[toTile].piece = moves[0][0];
+						}
 						break;
 					}
 				}
@@ -47,11 +68,7 @@ function GetChessboardMove(cmp: ChessBoardMoveProps) {
 		}
 	}
 }
-	// if(moves.length > 0) {
-	// 	console.log(moves);
-	// 	console.log(isBlack);
-	// 	console.log(newBoardState);
-	// }
+	
 
 	return ( {boardTiles: newBoardState});
 }
