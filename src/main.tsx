3d08b2e72c40977d9f6f19b564e0be00c5849761
Zokky2e <icro4.gaming@hotmail.com>
2023-06-05 18:20:07 +0200

    title of app

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <App />
-)
+document.title = "Chess Visualization";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<App />
+);
